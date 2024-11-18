82a03b0dfd6cdd8799339fe7ad77921a3880c3ed
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-11-18 09:08:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+#dev
+
+1. Instalar dependencias
+
+``` npm i ```
+
+2. Clonar el .env.template y crear el .env
+
+3. Ejecutar proyecto
+
+``` npm run dev ```
+
+4. abrir localhost o postman para revisar el correcto funcionamiento de la API
+
+localhost:puerto configurado en .env
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+
+const todos = [
+  { id: 1, text: "Buy milk", completedAt: new Date() },
+  { id: 2, text: "Buy bread", completedAt: null },
+  { id: 3, text: "Buy butter", completedAt: new Date() },
+];
+
+export class TodosController {
+  // DI
+  constructor() {}
+
+  public getTodos = (req: Request, res: Response) => {
+    res.json(todos);
+  };
+
+  public getTodoById = (req: Request, res: Response) => {
+    const id = +req.params.id;
+    if (isNaN(id)) {
+      res.status(400).json({ error: "ID argument is not a number" });
+    }
+
+    const todo = todos.find((todo) => todo.id === id);
+
+    todo
+      ? res.json(todo)
+      : res.status(404).json({ error: `TODO with id ${id} not found` });
+  };
+
+  public createTodo = (req: Request, res: Response) => {
+    const { text } = req.body;
+    if (!text) {
+      res.status(400).json({ error: "Text property is required" });
+    }
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+      completedAt: null,
+    };
+
+    todos.push(newTodo);
+
+    res.json(newTodo);
+  };
+
+  public updateTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+    if (isNaN(id)) {
+      res.status(400).json({ error: "ID argument is not a number" });
+    }
+
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) {
+      res.status(404).json({ error: `Todo with id ${id} not found` });
+    } else {
+      const { text, completedAt } = req.body;
+
+      todo.text = text || todo.text;
+      completedAt === "null"
+        ? (todo.completedAt = null)
+        : (todo.completedAt = new Date(completedAt || todo.completedAt));
+
+      res.json(todo);
+    }
+  };
+
+  public deleteTodo = (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) {
+      res.status(404).json({ error: `Todo with id ${id} not found` });
+    } else {
+      todos.splice(todos.indexOf(todo), 1);
+      res.json(todo);
+    }
+  };
+}
